7958e0c99b33e7671777b26d0c5f058773b87e9c
crunchysmoothie <ravanzyl2@gmail.com>
2024-10-07 21:19:30 +0200

    Updated files

--- portal_frame_analysis.py
+++ portal_frame_analysis.py
@@ -1,5 +1,6 @@
 from PyNite import FEModel3D
 from PyNite.Visualization import Renderer
+from PyNite import Reporting
 import member_database as mdb
 import json
 
@@ -15,11 +16,11 @@ def import_data(file):
                                       'RX': support['RX'], 'RY': support['RY'], 'RZ': support['RZ']}
                     for support in data.get('supports', [])}
 
-        node_loads = {node_load['node']: {'direction': node_load['direction'], 'magnitude': node_load['magnitude']}
+        node_loads = {node_load['node']: {'direction': node_load['direction'], 'magnitude': node_load['magnitude'], 'case': node_load['case']}
                       for node_load in data.get('nodal_loads', [])}
 
         member_loads = {member_load['member']: {'direction': member_load['direction'],
-                                                'w1': member_load['w1'], 'w2': member_load['w2']}
+                                                'w1': member_load['w1'], 'w2': member_load['w2'], 'case': member_load['case']}
                         for member_load in data.get('member_loads', [])}
 
         materials = {material['name']: {'E': material['E'], 'G': material['G'],
@@ -45,7 +46,8 @@ def lightest_section():
         frame.add_node(node, data['nodes'][node]['x'], data['nodes'][node]['y'], data['nodes'][node]['z'])
 
     for support in data['supports']:
-        frame.def_support(support, data['supports'][support]['DX'], data['supports'][support]['DY'],)
+        frame.def_support(support, data['supports'][support]['DX'], data['supports'][support]['DY'], data['supports'][support]['DZ'],
+                          data['supports'][support]['RX'], data['supports'][support]['RY'], data['supports'][support]['RZ'])
 
     for material in data['materials']:
         frame.add_material(material, data['materials'][material]['E'], data['materials'][material]['G'],
@@ -74,22 +76,29 @@ def lightest_section():
     frame.add_member('M4', 'N4', 'N5', 'Steel_S355', CIy, CIx, CJ, CA)
 
     for node_load in data['nodal_loads']:
-        frame.add_node_load(node_load, data['nodal_loads'][node_load]['direction'], data['nodal_loads'][node_load]['magnitude'])
+        frame.add_node_load(node_load, data['nodal_loads'][node_load]['direction'], data['nodal_loads'][node_load]['magnitude'], data['nodal_loads'][node_load]['case'])
 
     for member_load in data['member_loads']:
         frame.add_member_dist_load(member_load, data['member_loads'][member_load]['direction'],
-                                 data['member_loads'][member_load]['w1'], data['member_loads'][member_load]['w2'])
+                                 data['member_loads'][member_load]['w1'], data['member_loads'][member_load]['w2'],
+                                   None, None, data['member_loads'][member_load]['case'])
+
+    frame.add_load_combo('Combo 1', factors={'D': 1.0, 'L': 1.0})
+    frame.add_load_combo('1.0 DL', factors={'D': 1.0, 'L': 0})
+    frame.add_load_combo('1.2 DL + 1.6 LL', factors={'D': 1.2, 'L': 1.6})
+
+    frame.add_member_self_weight('FY', 1, 'D')
 
     # Analyze the model
     frame.analyze(check_statics=True)
 
-    # Render the deformed shape
-    rndr = Renderer(frame)
-    rndr.annotation_size = 250
-    rndr.render_loads = True
-    rndr.deformed_shape = True
-    rndr.deformed_scale = 100
-    rndr.render_model()
+    # # Render the deformed shape
+    # rndr = Renderer(frame)
+    # rndr.annotation_size = 250
+    # rndr.render_loads = True
+    # rndr.deformed_shape = True
+    # rndr.deformed_scale = 100
+    # rndr.render_model()
 
     print("Member M1 Max Mz:", frame.members['M1'].max_moment('Mz') / 1000, "kN-m")
     print("Member M1 Min Mz:", frame.members['M1'].min_moment('Mz') / 1000, "kN-m")
@@ -99,9 +108,8 @@ def lightest_section():
     print("Node N3 DX Displacement:", frame.nodes['N3'].DX, "mm")
     print("Node N4 DZ Displacement:", frame.nodes['N4'].DX, "mm")
 
+
     return
 
 lightest_section()
 # data = import_data('input_data.json')
-# for member_load in data['member_loads']:
-#     print(type(member_load))
